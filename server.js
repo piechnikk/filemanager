3d3a11ad546b3abdd31e1297d4b7194143da260c
piechnikk <piechnik10@gmail.com>
2020-12-25 14:44:32 +0100

    fix download problem (I hope)

--- server.js
+++ server.js
@@ -36,10 +36,14 @@ app.post('/handleUpload', function (req, res) {
     form.parse(req, function (err, fields, files) {
         if (files.imagetoupload.length > 1) {
             for (let i = 0; i < files.imagetoupload.length; i++) {
+                var path = ""
+                for (let j = files.imagetoupload[i].path.split("\\").length - 2; j < files.imagetoupload[i].path.split("\\").length; j++) path += "/" + files.imagetoupload[i].path.split("\\")[j]
                 filesTab.push({ id: id, name: files.imagetoupload[i].name, path: files.imagetoupload[i].path, size: files.imagetoupload[i].size, type: files.imagetoupload[i].type, savedate: new Date().getTime() })
                 id++
             }
         } else {
+            var path = ""
+            for (let j = files.imagetoupload.path.split("\\").length - 2; j < files.imagetoupload.path.split("\\").length; j++) path += "/" + files.imagetoupload.path.split("\\")[j]
             filesTab.push({ id: id, name: files.imagetoupload.name, path: files.imagetoupload.path, size: files.imagetoupload.size, type: files.imagetoupload.type, savedate: new Date().getTime() })
             id++
         }
