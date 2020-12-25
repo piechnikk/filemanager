9c7b2621208c0de2bea9825212a3f5268d972847
piechnikk <piechnik10@gmail.com>
2020-12-25 17:48:36 +0100

    fix image problem

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const { type } = require("os")
 
 var filesTab = []
 var id = 1
+const extTab = ["doc", "docx", "exe", "gif", "html", "iso", "jpeg", "jpg", "mp3", "mp4", 'mpeg', "msi", "pdf", 'png', "ppt", 'rar', 'raw', "sql", 'svg', 'tiff', "txt", "wav", "xls", "zip"]
 
 app.get("/", function (req, res) {
     res.redirect("/upload")
@@ -37,14 +38,22 @@ app.post('/handleUpload', function (req, res) {
         if (files.imagetoupload.length > 1) {
             for (let i = 0; i < files.imagetoupload.length; i++) {
                 var path = ""
+                var extension = "undefined"
+                for (let j = 0; j < extTab.length; j++) {
+                    if (files.imagetoupload[i].path.split(".")[1].toLowerCase() == extTab[j]) extension = extTab[j].toUpperCase()
+                }
                 for (let j = files.imagetoupload[i].path.split("/").length - 2; j < files.imagetoupload[i].path.split("/").length; j++) path += "/" + files.imagetoupload[i].path.split("/")[j]
-                filesTab.push({ id: id, name: files.imagetoupload[i].name, path: files.imagetoupload[i].path, size: files.imagetoupload[i].size, type: files.imagetoupload[i].type, savedate: new Date().getTime(), download: path, extension: files.imagetoupload[i].path.split(".")[1] })
+                filesTab.push({ id: id, name: files.imagetoupload[i].name, path: files.imagetoupload[i].path, size: files.imagetoupload[i].size, type: files.imagetoupload[i].type, savedate: new Date().getTime(), download: path, extension: extension })
                 id++
             }
         } else {
+            var extension = "undefined"
+            for (let j = 0; j < extTab.length; j++) {
+                if (files.imagetoupload.path.split(".")[1].toLowerCase() == extTab[j]) extension = extTab[j].toUpperCase()
+            }
             var path = ""
             for (let j = files.imagetoupload.path.split("/").length - 2; j < files.imagetoupload.path.split("/").length; j++) path += "/" + files.imagetoupload.path.split("/")[j]
-            filesTab.push({ id: id, name: files.imagetoupload.name, path: files.imagetoupload.path, size: files.imagetoupload.size, type: files.imagetoupload.type, savedate: new Date().getTime(), download: path, extension: files.imagetoupload.path.split(".")[1] })
+            filesTab.push({ id: id, name: files.imagetoupload.name, path: files.imagetoupload.path, size: files.imagetoupload.size, type: files.imagetoupload.type, savedate: new Date().getTime(), download: path, extension: extension })
             id++
         }
         console.log(filesTab);
